c3678b812b1e40954e371dfc7eb5427dd4ed2672
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2026-05-15 15:29:12 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -105,6 +105,7 @@ class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
 class DriverLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Driver
     form_class = DriverLicenseUpdateForm
+    success_url = reverse_lazy("taxi:driver-list")
 
 
 @login_required
